915be0d0580d095c34e5f4fddf8a95c838d3115a
Dhruv Roy Talukdar <dhruvroy8@gmail.com>
2020-07-02 16:22:53 +0530

    My first commit. Only the bubble sort algorithm is implemented. Work to be done: 1>Other sorting algorithm 2>Introduce color make the GUI more color full.

---
+++ src/sortvisualizer/NewClass.java
@@ -0,0 +1,11 @@
+package sortvisualizer;
+
+import javax.swing.*;
+
+public class NewClass {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+            new SwingDemo();
+        });
+    }
+}
